51352c7771d4cb7b2469d221c512af2361a6aa58
Madeline Celis <celiscb@vcu.edu>
2018-09-15 19:07:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendDriver(name){
+  drivers.push(name);
+};
+
+function destructivelyPrependDriver(name){
+  drivers.unshift(name);
+};
+
+function destructivelyRemoveLastDriver(name){
+  drivers.pop();
+};
+
+function destructivelyRemoveFirstDriver(){
+  drivers.shift();
+};
+
+function appendDriver(name){
+  const copy = [...drivers, name];
+  return copy;
+};
+
+function prependDriver(name){
+  const copy = [name, ...drivers];
+  return copy;
+};
+
+function removeLastDriver(){
+  let copy = drivers.slice();
+  return copy.slice(0, copy.length -1);
+};
+
+function removeFirstDriver(){
+  let copy = drivers.slice();
+  return copy.slice(1);
+};
